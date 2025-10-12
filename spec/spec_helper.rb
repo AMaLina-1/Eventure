2501e9354768b837708f80f574e3e7db040f5687
gelicheng <geli@LAPTOP-BL2BC8TS.>
2025-10-12 17:01:46 +0800

    add spech_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+# frozen string_literal: true
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
+require 'yaml'
+
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/rg'
+
+require 'vcr'
+require 'webmock'
+
+require_relative '../lib/hccg/http_request'
+
+TOP = 10
+CONFIG = YAML.safe_load_file('config/secrets.yml') if File.exist?('config/secrets.yml')
+# API_KEY = CONFIG['API_KEY']
+CORRECT = YAML.safe_load_file('spec/fixtures/results.yml')
+
+CASSESTTES_FOLDER = 'spec/fixtures/cassettes'
+CASSETE_FILE = 'hccg_api'
