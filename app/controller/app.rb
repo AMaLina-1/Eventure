6a99629402b7f0c5287e78bc0fd63982a819e346
gelicheng <geli@LAPTOP-BL2BC8TS.>
2025-10-18 22:38:00 +0800

    updated app.rb file

--- app/controller/app.rb
+++ app/controller/app.rb
@@ -15,41 +15,16 @@ module Eventure
       response['Content-Type'] = 'text/html; charset=utf-8'
 
       routing.root do
-        view 'home'
+        routing.redirect '/activities'
       end 
 
-      routing.on 'project' do
+      routing.on 'activities' do
         routing.is do 
-          routing.post do 
-            # will edit this later on 
-            category = routing.params['category']
-            id = routing.params['id']
-            routing.halt 400, "Missing params" if category.nil? || id.nil?
-            routing.redirect "hccg/#{category}/#{id}"
-          end 
-        end 
-        routing.on String, String do |category, id|
           routing.get do 
-            # will edit this later on 
-            project = Hccg::Repository::For [Hccg::Entity::Project].find_id(category: category, id: id)
-            members = Hccg::Repository::For [Hccg::Entity::Member].find_project_id(category: category, id: id)
-
-            project ||= OpenStruct.new(
-              subject: '新竹市「114年度勞動三法宣導會」，自即日起開放報名!',
-              detail: 'something about the project',
-              subject_class: [],
-              service_class: [],
-              place: '',
-              start_time: '',
-              end_time: '',
-            )
-
-            members ||=[
-              OpenStruct.new(name: ''),
-              OpenStruct.new(name: '')
-            ]
-
-            view 'project', locals: { project: project, members: members }
+            mapper = Eventure::Hccg::ActivityMapper.new
+            activities = mapper.find(10).map(&:to_entity)
+          
+            view 'home', locals: { cards: activities, total_pages: 1, current_page: 1 }
           end
         end
       end 
