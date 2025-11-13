39a52dbd6cbf89beac9ad0cd6bf9a08d887e651c
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2025-11-13 23:04:38 +0800

    add: like page and let filter connect session

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -21,16 +21,33 @@ module Eventure
       # root route
       routing.root { routing.redirect '/activities' }
 
+      # likes page (shows user's liked activities)
+      routing.get 'like' do
+        liked_sernos = Array(session[:user_likes]).map(&:to_i)
+        liked_activities = liked_sernos.map { |s| Eventure::Repository::Activities.find_serno(s) }.compact
+
+        # render like view with the same card component data shape
+        view 'like',
+             locals: view_locals.merge(cards: Views::ActivityList.new(liked_activities), liked_sernos: liked_sernos)
+      end
+
       # activities route
       routing.on 'activities' do
         # GET /activities
         routing.is do
-          begin
-            show_activities(100)
-          rescue StandardError => e
-            flash[:error] = "Error loading activities: #{e.message}"
-            routing.redirect '/'
+          if routing.params['filter_tag'] || routing.params['filter_city']
+            session[:filters] = {
+              tag: Array(routing.params['filter_tag'] || routing.params['filter_tag[]']).map(&:to_s).reject(&:empty?),
+              city: routing.params['filter_city']&.to_s
+            }.compact
+          else
+            session.delete(:filters)
           end
+
+          show_activities(100)
+        rescue StandardError => e
+          flash[:error] = "Error loading activities: #{e.message}"
+          routing.redirect '/'
         end
 
         # update likes
@@ -68,17 +85,39 @@ module Eventure
     end
 
     # show activites page
-    def show_activities(top)
+    def show_activities(_top)
       # get activities from service
       # activities = service.search(top, Eventure::Entity::TempUser.new(user_id: 1))
-      if activities.nil? || activities.empty?
+      all = activities
+      if all.nil? || all.empty?
         flash[:notice] = 'No activities available'
         return
       end
 
+      # apply filters stored in session
+      filters = session[:filters] || {}
+      filtered = all.dup
+
+      if filters[:tag] && !filters[:tag].empty?
+        tag_set = Array(filters[:tag]).map(&:to_s)
+        filtered = filtered.select do |a|
+          Array(a.tags).map { |t| t.tag.to_s }.intersect?(tag_set)
+        end
+      end
+
+      if filters[:city] && !filters[:city].to_s.empty?
+        city = filters[:city].to_s
+        filtered = filtered.select do |a|
+          # attempt to read city from activity.location if available
+          a.respond_to?(:location) && a.location && a.location.city && a.location.city.to_s == city
+        end
+      end
+
       liked = Array(session[:user_likes]).map(&:to_i)
-      @filtered_activities = activities
-      @tags = activities.flat_map { |activity| extract_tags(activity) }.uniq
+      @filtered_activities = filtered
+      @tags = all.flat_map { |activity| extract_tags(activity) }.uniq
+      @current_filters = filters
+
       view 'home', locals: view_locals.merge(liked_sernos: liked)
     end
 
@@ -88,7 +127,7 @@ module Eventure
 
     def view_locals
       {
-        cards: Views::ActivityList.new(activities),
+        cards: Views::ActivityList.new(@filtered_activities || activities),
         total_pages: 1,
         current_page: 1
       }
